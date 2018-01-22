2329e7f643359d29ab8c250fd8a2881c759e3d9a
wfh0429 <872490934@qq.com>
2018-01-22 23:17:09 +0800

    2018.1.22

--- sogou.py
+++ sogou.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.common.exceptions import StaleElementReferenceException
 
 # <img src="https://pics.onsizzle.com/diogenes-5h-the-whole-i-boned-your-mom-schtick-really-stings-30227419.png" class="mainImage" title="查看源图" alt="mom 的图像结果">
 # 
@@ -21,7 +21,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 contents = ['哥哥弟弟','爸爸儿子','妈妈女儿','表哥表弟','表姐表妹','兄弟姐妹','家庭合照','全家福','三胞胎','四胞胎','三弟兄']
 for word in contents:
 	driver = webdriver.Chrome()
-	driver.get('https://cn.bing.com/images/search?view=detailV2&q='+word+'&first=1&selectedindex=1')
+	driver.get('https://pic.sogou.com/d?query='+word+'&mode=1&did=1#did1')
 	from time import sleep 
 	kw = driver.find_element_by_id("btnPgRgt")
 	n = 0
@@ -36,20 +36,22 @@ for word in contents:
 			# driver.close()
 			pass
 		imgs = driver.find_elements(By.TAG_NAME, 'img')
-		# print(len(imgs))
+		print(len(imgs))
 		last = None
 		for img in imgs:
 			try:	
-				if img.get_attribute('class') == 'mainImage accessible nofocus': 
+				if img.get_attribute('title') == '点击查看源网页': 
 					print(img.get_attribute('src'))
 					r = requests.get(img.get_attribute('src'), stream=True,timeout=5)
 					if r.status_code == 200: 
 						imgtype = img.get_attribute('src')[-3:]
-						with open('/home/wangfeihong/bing/'+word+str(n)+'.'+imgtype,'wb') as f:
+						with open('/home/wangfeihong/sogou/'+word+str(n)+'.'+imgtype,'wb') as f:
 							for chunk in r.iter_content(1024): 
 								f.write(chunk)
 							n = n + 1
 							print(word+str(n)+'.'+imgtype)
+			except StaleElementReferenceException:
+				pass	
 			except Exception as e:
 				print(e)
 				pass
